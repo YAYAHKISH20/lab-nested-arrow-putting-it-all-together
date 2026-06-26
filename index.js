421032506d581f085d3dc2b9553bfa2c0317b993
panther <panther.debra@student.moringaschool.com>
2026-06-26 15:10:00 +0300

    fix login failed message

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const loginAttempt = (passwordAttempt) => {
+    attemptCount++;
 
+    if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+  }; 
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+  return loginAttempt; 
+}
+
+// Testing the function
+const trackLogin = createLoginTracker("user123");
+console.log(trackLogin("wrong_password")); 
+console.log(trackLogin("wrong_password_2"));
+console.log(trackLogin("wrong_password_3"));
+console.log(trackLogin("wrong_password_4"));
